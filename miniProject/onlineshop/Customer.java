70b079e27607b780911bfa8d897c01372029a3ac
hazim101205 <zulkanainhazim@gmail.com>
2026-06-29 02:34:20 +0800

    settle part aku

--- miniProject/onlineshop/Customer.java
+++ miniProject/onlineshop/Customer.java
@@ -1,3 +1,33 @@
 package miniProject.onlineshop;
 
+import java.util.ArrayList;
 
+public class Customer extends User{
+
+    private String address;
+    private ArrayList<Item> shoppingCart;
+
+     public Customer(String userId, String name, String email, String address) {
+        super(userId, name, email);
+        this.address = address;
+        this.shoppingCart = new ArrayList<>();
+    }
+
+    public void addToCart(Item item) {
+        shoppingCart.add(item);
+    }
+
+    public ArrayList<Item> getShoppingCart() {
+        return shoppingCart;
+    }
+
+    @Override
+    public void displayProfile() {
+        System.out.println("===== CUSTOMER PROFILE =====");
+        System.out.println("Customer ID : " + userId);
+        System.out.println("Name        : " + name);
+        System.out.println("Email       : " + email);
+        System.out.println("Address     : " + address);
+        System.out.println("Cart Items  : " + shoppingCart.size());
+    }
+}
--- miniProject/onlineshop/User.java
+++ miniProject/onlineshop/User.java
@@ -1,4 +1,20 @@
 package miniProject.onlineshop;
-public class User {
+
+public abstract class User {
     
+    protected String userId;
+    protected String name;
+    protected String email;
+
+    public User(String userId, String name, String email) {
+        this.userId = userId;
+        this.name = name;
+        this.email = email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public abstract void displayProfile();
 }
